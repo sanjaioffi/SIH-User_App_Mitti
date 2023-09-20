d2f40edf0e95bc3fec8d6c2cf09f754092cc1959
sundhar22 <s.mohanasundharam2210@gmail.com>
2023-09-20 20:24:17 +0530

    just pull

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,16 @@ import 'package:user_mitti/screens/call/videocall/videocall.dart';
 import 'package:user_mitti/screens/call/voicecall/voicecall.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
+import 'package:user_mitti/screens/home_page.dart';
+import 'package:user_mitti/screens/main_page.dart';
+import 'package:user_mitti/screens/signin_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-   options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
   await Firebase.initializeApp();
   runApp(MyApp());
 }
@@ -30,9 +33,13 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
           useMaterial3: true,
         ),
-        home: GroupCall(),
+        home: MainPage(),
         routes: {
-          "voiceCall": (p0) => VoiceCall("cha" ),
+          "groupCall": (p0) => GroupCall(),
+          "voiceCall": (p0) => VoiceCall("cha"),
+          "mainPage": (p0) => MainPage(),
+          "homePage": (p0) => HomePage(),
+          "signInPage": (p0) => SignInPage(),
           "videoCall": (p0) => VideoCall(),
         },
       ),
